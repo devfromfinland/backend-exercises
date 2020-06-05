7d555b280b8779fcdc07ca807dadd5f3ca549a72
devfromfinland <contact@vietphan.info>
2020-06-05 18:10:31 +0300

    add front-end build

---
+++ build/precache-manifest.0a42fe0257981a36f27d1739c0b7b3d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540c8d1562776ad1acf7fcea093b04cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd40a2b49361b866971c",
+    "url": "/static/css/main.89e8e642.chunk.css"
+  },
+  {
+    "revision": "c2db7b13b6c244651373",
+    "url": "/static/js/2.d5e710d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5e710d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd40a2b49361b866971c",
+    "url": "/static/js/main.4b4cfc6e.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
