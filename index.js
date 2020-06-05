584537f9e3c83439bef4b3a7f9763d614a67cf98
devfromfinland <contact@vietphan.info>
2020-06-05 17:52:25 +0300

    test: revert back the '/' route

--- index.js
+++ index.js
@@ -23,27 +23,6 @@ app.use(morgan((tokens, req, res) => {
   ].join(' ')
 }))
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
 let persons = [
   {
     "name": "Arto Hellas",
@@ -74,9 +53,9 @@ app.get('/info', (req, res) => {
   res.send(text)
 })
 
-app.get('/', (req, res) => {
-  res.send('<h1>This is the backend code for phonebook exercise</h1>')
-})
+// app.get('/', (req, res) => {
+//   res.send('<h1>This is the backend code for phonebook exercise</h1>')
+// })
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
