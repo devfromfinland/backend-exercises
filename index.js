3d1720cce637b87c5df7164c968789d0a49885ec
devfromfinland <contact@vietphan.info>
2020-06-05 17:08:00 +0300

    submit: prepare to publish to heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan((tokens, req, res) => {
   // console.log('tokens', tokens)
   // console.log('req', req.body)
@@ -138,7 +140,7 @@ app.post('/api/persons', (req, res) => {
   res.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
